435fc999dfe2e2625338cb9f2ca6bda95806aa7a
joaoescribano <joao.escribano@gmail.com>
2014-09-01 22:23:42 -0300

    Implementação de multiplos tipos de frete em um cálculo

--- app/code/local/TriForce/FreteReal/Model/Carrier.php
+++ app/code/local/TriForce/FreteReal/Model/Carrier.php
@@ -15,12 +15,14 @@ class TriForce_FreteReal_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstr
             $mao_propria = Mage::getStoreConfig('carriers/triforce_fretereal/mao_propria');
             $aviso_recebimento = Mage::getStoreConfig('carriers/triforce_fretereal/aviso_recebimento');
             $valor_declarado = Mage::getStoreConfig('carriers/triforce_fretereal/valor_declarado');
+            $extra_days = Mage::getStoreConfig('carriers/triforce_fretereal/extra_days');
 
             if ($client_id == "" || $client_secret == "") {
                 return false;
             }
 
-            $fretes = explode(",", Mage::getStoreConfig('carriers/triforce_fretereal/shipping_methods'));
+            $fretes = Mage::getStoreConfig('carriers/triforce_fretereal/shipping_methods');
+
             if ($fretes == "") {
                 return false;
             }
@@ -30,94 +32,92 @@ class TriForce_FreteReal_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstr
 
             $shippingAddress = $cart->getShippingAddress();
             $zip = $shippingAddress->getPostcode();
-            foreach ($fretes as $freteKey => $freteCod) {
-                $dadosParaAPI = array(
-                    'client_id' => $client_id,
-                    'client_secret' => $client_secret,
-                    'cep' => $zip,
-                    'forma_envio' => $freteCod,
-                    'mao_propria' => ($mao_propria ? "S" : "N"),
-                    'aviso_recebimento' => ($aviso_recebimento ? "S" : "N"),
-                    'produtos' => array()
+            $dadosParaAPI = array(
+                'client_id' => $client_id,
+                'client_secret' => $client_secret,
+                'cep' => $zip,
+                'forma_envio' => $fretes,
+                'mao_propria' => ($mao_propria ? "S" : "N"),
+                'aviso_recebimento' => ($aviso_recebimento ? "S" : "N"),
+                'produtos' => array()
+            );
+            $hash .= $zip;
+
+            foreach ($cart->getAllVisibleItems() as $key => $item) {
+                // Dados dos produtos dentro do carrinho
+                $product_sku = $item->getProduct()->getSku();
+                $product_name = $item->getProduct()->getName();
+                $product_price = $item->getProduct()->getPrice();
+                $product_weight = $item->getProduct()->getWeight();
+                $product_qtd = $item->getQty();
+
+                $hash .= $product_sku.$product_qtd;
+
+                $_product= Mage::getSingleton('catalog/product')->load($item->getProductId());
+                $product_c = $_product->getResource()->getAttribute('package_length')->getFrontend()->getValue($_product);
+                $product_l = $_product->getResource()->getAttribute('package_width')->getFrontend()->getValue($_product);
+                $product_a = $_product->getResource()->getAttribute('package_height')->getFrontend()->getValue($_product);
+
+                $dadosParaAPI['produtos'][] = array(
+                    'nome' => $product_name,
+                    'comprimento' => $product_c,
+                    'largura' => $product_l,
+                    'altura' => $product_a,
+                    'peso' => $product_weight,
+                    'qtd' => $product_qtd
                 );
 
-                $hash .= $zip;
-
-                foreach ($cart->getAllVisibleItems() as $key => $item) {
-                    // Dados dos produtos dentro do carrinho
-                    $product_sku = $item->getProduct()->getSku();
-                    $product_name = $item->getProduct()->getName();
-                    $product_price = $item->getProduct()->getPrice();
-                    $product_weight = $item->getProduct()->getWeight();
-                    $product_qtd = $item->getQty();
-
-                    $hash .= $product_sku.$product_qtd;
-
-                    $_product= Mage::getSingleton('catalog/product')->load($item->getProductId());
-                    $product_c = $_product->getResource()->getAttribute('package_length')->getFrontend()->getValue($_product);
-                    $product_l = $_product->getResource()->getAttribute('package_width')->getFrontend()->getValue($_product);
-                    $product_a = $_product->getResource()->getAttribute('package_height')->getFrontend()->getValue($_product);
-
-                    $dadosParaAPI['produtos'][] = array(
-                        'nome' => $product_name,
-                        'comprimento' => $product_c,
-                        'largura' => $product_l,
-                        'altura' => $product_a,
-                        'peso' => $product_weight,
-                        'qtd' => $product_qtd
-                    );
-
-                    if ($valor_declarado) {
-                        $dadosParaAPI['produtos'][$key]['valor'] = $product_price;
-                    }
-                }
-
-                if (isset($_SESSION['fretereal']) && $_SESSION['fretereal']['hash'][$freteCod] == $hash) {
-                    $ret = $_SESSION['fretereal'][$freteCod];
-                } else {
-                    $caminhoUrl = "https://fretereal.com/oauth/";
-                    $caminhoApi = $caminhoUrl . "action/api";
-                    $caminhoToken = $caminhoUrl . "action/request_token";
-
-                    $curl = curl_init($caminhoToken);
-                    curl_setopt($curl, CURLOPT_POST, true);
-                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, array(
-                        'client_id' => $client_id,
-                        'client_secret' => $client_secret,
-                        'grant_type' => 'client_credentials'
-                        )
-                    );
-
-                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                    $retToken = curl_exec($curl);
-                    $auth = json_decode($retToken);
-                    $access_key = $auth->access_token;
-
-                    $curl2 = curl_init($caminhoApi . "?access_token=" . $access_key);
-                    curl_setopt($curl2, CURLOPT_POST, true);
-                    curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
-                    curl_setopt($curl2, CURLOPT_POSTFIELDS, http_build_query($dadosParaAPI));
-                    curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
-                    $ret = curl_exec($curl2);
-                    $ret = json_decode($ret, true);
-
-                    $_SESSION['fretereal']['hash'][$freteCod] = $hash;
-                    $_SESSION['fretereal'][$freteCod] = $ret;
-                }
-
-                if ($ret['status'] == 1) {
-                    $method = Mage::getModel('shipping/rate_result_method');
-                    $method->setCarrier("triforce_fretereal")
-                                ->setCarrierTitle(Mage::getStoreConfig("carrier/triforce_fretereal/title"))
-                                ->setMethod($freteCod)
-                                ->setMethodTitle(self::getTitulo($freteCod))
-                                ->setCost($ret['fretes']['correios'][0]['valor'])
-                                ->setPrice($ret['fretes']['correios'][0]['valor']);
-                    $result->append($method);
+                if ($valor_declarado) {
+                    $dadosParaAPI['produtos'][$key]['valor'] = $product_price;
                 }
             }
 
+            if (isset($_SESSION['fretereal']) && $_SESSION['fretereal']['hash'] == $hash) {
+                $ret = $_SESSION['fretereal']['calculo'];
+            } else {
+                $caminhoUrl = "http://fretereal.com/oauth/";
+                $caminhoApi = $caminhoUrl . "action/api";
+                $caminhoToken = $caminhoUrl . "action/request_token";
+
+                $curl = curl_init($caminhoToken);
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, array(
+                    'client_id' => $client_id,
+                    'client_secret' => $client_secret,
+                    'grant_type' => 'client_credentials'
+                    )
+                );
+
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                $retToken = curl_exec($curl);
+                $auth = json_decode($retToken);
+                $access_key = $auth->access_token;
+
+                $curl2 = curl_init($caminhoApi . "?access_token=" . $access_key);
+                curl_setopt($curl2, CURLOPT_POST, true);
+                curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl2, CURLOPT_POSTFIELDS, http_build_query($dadosParaAPI));
+                curl_setopt($curl2, CURLOPT_RETURNTRANSFER, 1);
+                $ret = curl_exec($curl2);
+                $ret = json_decode($ret, true);
+
+                $_SESSION['fretereal']['hash'] = $hash;
+                $_SESSION['fretereal']['calculo'] = $ret;
+            }
+
+            if ($ret['status'] == 1) {
+                foreach ($ret['fretes']['correios'] as $key => $value) {
+                    $method = Mage::getModel('shipping/rate_result_method');
+                    $method->setCarrier("triforce_fretereal")
+                                ->setCarrierTitle(Mage::getStoreConfig("carrier/triforce_fretereal/title"))
+                                ->setMethod($value['tipo_frete'])
+                                ->setMethodTitle(self::getTitulo($value['tipo_frete']) . " (Prazo: ".($extra_days > 0 ? ($value['prazo'] + $extra_days) : $value['prazo'])." dias)")
+                                ->setCost($value['valor'])
+                                ->setPrice($value['valor']);
+                    $result->append($method);
+                }
+            }
         } catch (Exception $e) {
             $this->_appendError($result, $e->getMessage());
             return $result;
